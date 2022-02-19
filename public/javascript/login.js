c256a14f312500033115de1b473e458b8f2ca685
Emily Dokken <dokkenemily@gmail.com>
2022-02-19 13:24:40 -0800

    add signup functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -23,4 +23,36 @@ async function loginFormHandler(event) {
     }
 }
 
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const firstName = document.querySelector('#first-name').value.trim();
+    const lastName = document.querySelector('#last-name').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (firstName && lastName && username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
