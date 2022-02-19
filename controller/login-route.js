bbdc4cd2b346a039c60127f3b8138c2674f1b528
Emily Dokken <dokkenemily@gmail.com>
2022-02-18 19:13:55 -0800

    add login html and styles

--- controller/login-route.js
+++ controller/login-route.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 // '/login'
 router.get('/', (req, res) => {
     console.log('LOGIN route');
-    res.json({message: 'This will be the login page.'});// <------ REMOVE THIS LINE WHEN HANDLEBARS PAGE IS READY
     res.render('login');
 })
 
