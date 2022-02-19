90f714ac63748ad3b5262a3fa0411c4fcafdfd4a
CKURZ <ckurz098@gmail.com>
2022-02-18 19:04:12 -0800

    added code to send first letter of username for user icon

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controller');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
