1f2f80652ca434776b38c68f84ed4c5b88f47dd3
CKURZ <ckurz098@gmail.com>
2022-02-18 19:03:10 -0800

    added code to send first letter of username for user icon

--- controller/home-route.js
+++ controller/home-route.js
@@ -5,7 +5,6 @@ const { withAuth, testSession } = require('../utils/auth');
 router.get('/',testSession, withAuth,  async (req, res) => {
 //get all the plants and session user data
    const { plants, user, userPlants, firstLetter } = await getDB(req);
-console.log(firstLetter);
    //this will render the specified view INSIDE main.handlebars at {{{body}}}
    res.render('homepage', { plants, user, userPlants, firstLetter } );
 });
