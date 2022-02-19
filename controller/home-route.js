0de993d945f15133ec27d208f11e8e9cbe4f1319
CKURZ <ckurz098@gmail.com>
2022-02-18 16:53:30 -0800

    cleaned up code. prepare to merge

--- controller/home-route.js
+++ controller/home-route.js
@@ -5,10 +5,9 @@ const { withAuth, testSession } = require('../utils/auth');
 router.get('/',testSession, withAuth,  async (req, res) => {
 //get all the plants and session user data
    const { plants, user, userPlants } = await getDB(req);
-   
+
    //this will render the specified view INSIDE main.handlebars at {{{body}}}
    res.render('homepage', { plants, user, userPlants } );
-   // res.render('test', { plants, users, userPlants });
 });
 
 module.exports = router;
