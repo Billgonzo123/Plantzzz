cf4835d97e9b9fa34303947808bdb28afdbaca4b
Emily Dokken <dokkenemily@gmail.com>
2022-02-19 13:09:08 -0800

    add logout functionality in logout.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,8 +4,6 @@ async function loginFormHandler(event) {
     const email = document.querySelector('#login-email').value.trim();
     const password = document.querySelector('#login-password').value.trim();
 
-    console.log(email, password);
-
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
