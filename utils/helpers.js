eab6dfa049c274785dc7007bec54d03a576ba4be
Emily Dokken <dokkenemily@gmail.com>
2022-02-19 20:40:20 -0800

    update handlebars for plant-card

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,14 @@
 
 module.exports = {
     //insert functions into this object to use in handlebars {{function param1 param 2}}
-    sample_function: (param1,param2) => {
-        return `Helper says: ${param1+param2}`;
-    }
+    // sample_function: (param1,param2) => {
+    //     return `Helper says: ${param1+param2}`;
+    // },
+
+    imageFile: commonName => {
+        console.log(commonName);
+        return commonName
+        .replace(' ','_')
+        .toLowerCase();
+      }
 }
