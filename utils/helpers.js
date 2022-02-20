f261c4d74ea29ae4e6e62e4ad23b22e912c5d931
CKURZ <ckurz098@gmail.com>
2022-02-20 15:53:21 -0800

    Watering tasks renedering complete!

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,39 @@
 //this must be required in server.js
 
 module.exports = {
-    //insert functions into this object to use in handlebars {{function param1 param 2}}
-    // sample_function: (param1,param2) => {
-    //     return `Helper says: ${param1+param2}`;
-    // },
+  imageFile: commonName => {
+    return commonName
+      .replace(' ', '_')
+      .toLowerCase();
+  },
+  //check userPlant for watering days
+  renderUserPlants: (userPlants) => {
+    let dates = [];
 
-    imageFile: commonName => {
-        return commonName
-        .replace(' ','_')
-        .toLowerCase();
-      }
+    for (i = 0; i < 14; i++) {
+      let newDate = new Date();
+      newDate.setDate(newDate.getDate() + i);
+
+      dates.push({ date: newDate.toLocaleDateString("en-US"), plants: [] });
+
+      userPlants.forEach(plant => {
+        //get inital water date
+        const firstWaterDate = new Date(plant.initial_water_date);
+        //get how many days since inital water date
+        const diff = getDifferenceInDays(newDate, firstWaterDate);
+        //if there is no remained, then current date is a day of watering
+        (diff % plant.watering_interval) ? false : dates[i].plants.push(plant.nickname);
+      })
+      if (!dates[i].plants.length) dates[i].plants.push('No Plants to Water');
+    }
+    console.log(dates);
+    return dates;
+  }
+
+};
+
+//get diff in days from inital water date
+function getDifferenceInDays(date1, date2) {
+  const diffInMs = Math.abs(date2 - date1);
+  return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
