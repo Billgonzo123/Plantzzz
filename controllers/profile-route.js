a3b3021ab9251a966fa04f80bc904a81db59c708
Abigail Oswald <abigailoswald@Abigails-MacBook-Pro.local>
2022-02-21 19:45:29 -0800

    placeholder image

--- controllers/profile-route.js
+++ controllers/profile-route.js
@@ -7,12 +7,12 @@ router.get('/:userPlantId', withAuth, async (req, res) => {
 const { plants, user, userPlants, firstLetter, loggedIn } = await getDB(req);
 //send this variable to the view. Gets the requested user plant from the array of user plants
 const userPlant = userPlants.find(e => e.id == req.params.userPlantId);
-    if (!userPlant) {
-        console.log('The current logged in user does not have a plant by this id');
-        res.redirect('/');
-    } else {
+    // if (!userPlant) {
+    //     console.log('The current logged in user does not have a plant by this id');
+    //     res.redirect('/');
+    // } else {
         res.render('plant-profile',{ plants, user, userPlant, firstLetter, loggedIn});
-    }
+    // }
 });
 
 module.exports = router;
