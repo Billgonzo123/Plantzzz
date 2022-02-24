b8e1463ec207c8fc1808fc1434db2aa56241e659
CKURZ <ckurz098@gmail.com>
2022-02-23 22:19:43 -0800

    enabled security checkReferer

--- utils/auth.js
+++ utils/auth.js
@@ -23,15 +23,15 @@ const userIdMatch = (req, res, next) => {
 
 
 const checkReferer = (req, res, next) => { 
-    next();
-    // if (req.get('referer') && req.get('referer').includes('plantzzz.herokuapp.com/')) {
-    //     next();
-    // } else {
-    //     console.log('UNKNOWN REFERER: ', req.get('referer'));
-    //     console.log('Attempt to call user data form unknown source')
-    //     res.json({message: 'Access Denied'})
-    // }
+    // next(); //<----enable this when testing in localhost
+    if (req.get('referer') && req.get('referer').includes('plantzzz.herokuapp.com/')) {
+        next();
+    } else {
+        console.log('UNKNOWN REFERER: ', req.get('referer'));
+        console.log('Attempt to call user data form unknown source')
+        res.json({message: 'Access Denied'})
+    }
 }
- ////------end test session login-----//
+
 
 module.exports = {withAuth, isAdmin, userIdMatch, checkReferer};
